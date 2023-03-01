5fda8cc24d4d5f54c6c6634e48bc3ce39a646ab2
Jabriel <15688641+jukrb0x@users.noreply.github.com>
2023-03-01 19:06:17 +0800

    workflow: prettier sort imports

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -3,4 +3,9 @@ module.exports = {
   semi: true,
   singleQuote: true,
   printWidth: 180,
+  pluginSearchDirs: ['.'],
+  plugins: ['@trivago/prettier-plugin-sort-imports'],
+  importOrder: ['<THIRD_PARTY_MODULES>', '^@/', '^[./]'],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
 };
